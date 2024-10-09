d52a1391b735cff397deec5f017bac3ef3064d99
Cleber Leao <c.leao@reply.com>
2024-10-09 16:22:02 -0300

    aula Django w3schools

---
+++ my_tennis_club/members/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Member
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "joined_date",)
+
+
+admin.site.register(Member, MemberAdmin)

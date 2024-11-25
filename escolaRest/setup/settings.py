a65dad8da6615657a6cd0a2834381c148755ecc2
cleberleao <cleberleao_14@hotmail.com>
2024-11-25 20:04:42 -0300

    allowed hosts settings

--- escolaRest/setup/settings.py
+++ escolaRest/setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eni-_f0bzidsu)vpu)2$@kw$x%3hqemwjs*&i$k@22sf&x-av=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['150.230.88.104', 'localhost', '127.0.0.1']
 
 
 # Application definition
